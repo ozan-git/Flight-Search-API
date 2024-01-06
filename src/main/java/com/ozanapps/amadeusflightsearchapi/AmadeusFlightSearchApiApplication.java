70ed603d8c2031c494a4cae04226cf433cff2c13
Orhan Ozan Yıldız <34062337+ozan-git@users.noreply.github.com>
2024-01-06 12:10:54 +0300

    Initial setup and configuration of Spring Boot application for Flight Search API

---
+++ src/main/java/com/ozanapps/amadeusflightsearchapi/AmadeusFlightSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.ozanapps.amadeusflightsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusFlightSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmadeusFlightSearchApiApplication.class, args);
+    }
+
+}
